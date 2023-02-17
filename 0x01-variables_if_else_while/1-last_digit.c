6a7a86d127f7dd08e1f8939a5a01381c3105a37a
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-17 11:36:36 -0800

    last digit getter

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,9 +15,9 @@ int main(void)
 	last_num = n % 10;
 	if (last_num > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
-	else if (last_num < 6)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
-	else
+	else if (last_num == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last_num);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
 	return (0);
 }
