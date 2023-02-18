f155270786426b50b570d611018a35bb623af9be
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-17 16:13:00 -0800

    Write a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print letters but skip e and q
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		if (letters  == 'e' || letters == 'q')
+			continue;
+		putchar(letters);
+	}
+	putchar(letters);
+	return (0);
+}
