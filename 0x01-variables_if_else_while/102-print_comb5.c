93ecd8274fe60be7f8213277f6a4ceb88b99ba9f
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-17 21:10:52 -0800

    Write a program that prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = 0; num2 < 9; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
 			for (num3 = 0; num3 < 10; num3++)
 			{
@@ -21,11 +21,12 @@ int main(void)
 					putchar(' ');
 					putchar(num3 + '0');
 					putchar(num4 + '0');
-					if (num1 < 9 || num2 < 8 || num3 < 9 || num4 < 8)
+					if (num1 = 9 && num2 == 8 && num3 == 9 && num4 == 8)
 					{
-						putchar(',');
-						putchar(' ');
+						continue;
 					}
+						putchar(',');
+						putchar(' ')
 				}
 			}
 		}
