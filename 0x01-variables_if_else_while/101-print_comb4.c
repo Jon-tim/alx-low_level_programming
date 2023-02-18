7b0929b13d296ed896887ecdba9863a76af886b8
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-17 20:28:17 -0800

    Write a program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,9 +17,9 @@ int main(void)
 			{
 				if (num1 < num2 && num2 < num3)
 				{
-					putchar(num1);
-					putchar(num2);
-					putchar(num3);
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(num3 + '0');
 					if (num1 == 7 && num2 == 8 && num3 == 9)
 					{
 						continue;
