ca08214134177dc30b457d7ffb96d5a6b3be9d0f
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-21 17:14:34 -0800

    Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,8 +14,10 @@ void jack_bauer(void)
 		for (b = 0; b <= 59; b++)
 		{
 			_putchar(a / 10 + 48);
+			_putchar(a % 10 + 48);
 			_putchar(':');
 			_putchar(b / 10 + 48);
+			_putchar(b % 10 + 48);
 			_putchar('\n');
 		}
 	}
