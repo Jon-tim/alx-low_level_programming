97184e1a5667da5d9ab5993380ea4db5be0efcf2
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-23 05:21:26 -0800

    fix error

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -11,6 +12,13 @@ int main(void)
 	/* {*/
 	/*	putchar(i);*/
 	/* }*/
+
+	/*  while (i < 10)*/
+	/* {*/
+	/*      putchar(i);*/
+	/* }*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
