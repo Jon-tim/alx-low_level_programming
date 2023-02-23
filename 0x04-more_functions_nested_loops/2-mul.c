b489457a1c1ae5f54cd77cb334a317c979fa87de
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-23 05:01:54 -0800

    Function that multiples two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 integers
+ * @a: parameter to be checked
+ * @b: parameter to be checked 
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
