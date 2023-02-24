04632fa70fdf0d78ebb8fc268d4e435456f8f8b7
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-23 16:50:56 -0800

    Print diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw diagonal line
+ * @n: parameter to be evaluated
+ * Return: /
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = i; j < n; j++)
+			{
+				_putchar(" ");
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
