2343a4c6ab5394a00734237e403f864e69c2f24d
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-24 16:54:37 -0800

    fix bug

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,5 +8,17 @@
 
 void print_number(int n)
 {
-	_putchar(n);
+	unsigned int number = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		number = -number;
+	}
+	
+	if ((number / 10) > 0)
+	{
+		print_number(number / 10);
+	}
+	_putchar((number % 10) + 48);
 }
