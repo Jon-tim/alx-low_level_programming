18958daec687c44883c553c284699723ad7b6dfa
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-27 08:00:17 -0800

    updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates a value through it's pointer
+ *
+ * @n: parameter to be evaluated
+ *
+ * Return: updated parameter
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a directory containing pointers, arrays and strings related concepts in C
