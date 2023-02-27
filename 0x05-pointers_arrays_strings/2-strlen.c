97596f255e9184d3b1244c1ee5668608974b77c6
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-27 13:34:29 -0800

    Get string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Evaluates the length of a string
+ *
+ * @c: string/parameter to be evaluated
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int length_of_str;
+	
+	length_of_str = strlen(s);
+	return (length_of_str);
+}
