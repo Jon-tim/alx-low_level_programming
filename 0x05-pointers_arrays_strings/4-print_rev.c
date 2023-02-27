30ccbc69845567f5691984ad99ca9a4036f941df
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-27 15:38:04 -0800

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - Reverse a string
+ *
+ * @s: parameter to be reversed
+ *
+ * Return: a reversed string
+ */
+
+void print_rev(char *s)
+{
+	if (strlen(s) > 0)
+	{
+		int count;
+
+		for (count = strlen(s); s[count] != '\0' && s[count] > 0; count--)
+		{
+			_putchar(s[count]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
