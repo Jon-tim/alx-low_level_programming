360c944a3429c0ff05481b8059c7bfbb23a6d43f
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-28 15:48:30 -0800

    print second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - print second half a string
+ *
+ * @str: string to be evaluated
+ *
+ * Return: second half of a string
+ */
+
+void puts_half(char *str)
+{
+	int length = 0, index, n;
+
+	while (str[length] != '\0')
+		length++;
+
+	if ((length % 2) == 0)
+		n = length / 2;
+	else
+		n = (len - 1) / 2;
+
+	for (index = n; index < length; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
