c3c114a540654f792b2a8f6996da656d3000b4d9
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-28 18:58:24 -0800

    string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ *@s: string to be evaluated
+ *
+ *Return: integer
+ *
+ */
+
+int _atoi(char *s)
+{
+	int count = 0, hold;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		if (s[count] >= 30 || s[count] <= 39 || s[count] == 43 || s[count] == 45)
+		{
+			_putchar(s[count]);
+		}
+	}
+}
