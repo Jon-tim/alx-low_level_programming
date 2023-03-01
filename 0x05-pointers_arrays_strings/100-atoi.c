78cfe11567d8f47467b3fdaba2054a2c4c76b37e
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-02-28 19:01:47 -0800

    string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,13 +9,14 @@
 
 int _atoi(char *s)
 {
-	int count = 0;
+	int count = 0, hold;
 
 	for (count = 0; s[count] != '\0'; count++)
 	{
 		if (s[count] >= 30 || s[count] <= 39 || s[count] == 43 || s[count] == 45)
 		{
-			_putchar(s[count]);
+			hold = s[count];
 		}
 	}
+	return (hold);
 }
