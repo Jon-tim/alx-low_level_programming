2e359db4f1ed68de43b2c81959dd07c0da955afa
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-02 15:37:55 -0800

    copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ *@dest: destination string
+ *@src: source string
+ *@n: byte amount
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
