a1f32ea4caa1a695c22b4a3e96e6c62ed7772309
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-02 10:41:08 -0800

    fix whitespace errors

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,9 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, len = 0;
-	
-	while(dest[len] != '\0')
+
+	while (dest[len] != '\0')
 		len++;
-	
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[len + i] = src[i];
