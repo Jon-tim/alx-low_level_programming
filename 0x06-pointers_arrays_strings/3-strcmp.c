6d97e9ae7c646f744eeb4908c5178d6acaf75022
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-02 15:58:50 -0800

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcmp - compare two string
+ * @s1: first string
+ * @s2: second string
+ * Return: 15, -15 or 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] < s2[i])
+		{
+			return (-15);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (15);
+		}
+		else
+			return (0);
+	}
+}
