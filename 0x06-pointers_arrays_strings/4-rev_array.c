91038f879df18d0942ca21d6c59984b23fc31c8f
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-02 19:22:07 -0800

    Change lowercase to uppercase

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,9 +10,9 @@ void reverse_array(int *a, int n)
 {
 	int i = 0, j = 0, temp;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		for (j = n; j > 0; j--)
+		for (j = i + 1; j > 0; j--)
 		{
 			temp = a[j];
 			a[j] = a[j - 1];
