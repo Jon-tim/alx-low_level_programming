0ea7c9e7491864aebcb298334210358acfa41b05
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-02 18:54:22 -0800

    Change lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *string_toupper - lowercase to uppercase
+ *@c: characters to be changed 
+ * Return: uppercase strings
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - 32;
+		}
+	}
+
+	return (c);
+}
