aa7c31075a46962eb4c169da092cb30491d5e17d
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-02 16:12:38 -0800

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,17 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1 != '\0' && s2 != '\0'; i++)
 	{
 		if (s1[i] < s2[i])
 		{
-			return (-15);
+			_putchar("-15");
 		}
 		else if (s1[i] > s2[i])
 		{
-			return (15);
+			_putchar("15");
 		}
 		else
-			return (0);
+			_putchar("0");
 	}
 }
