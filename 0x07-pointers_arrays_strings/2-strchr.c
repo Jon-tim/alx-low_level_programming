b208acba2d41d96ae802e44d5319caf81a401b63
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-06 02:56:36 -0800

    function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string
+ * @s: String to be evaluated
+ * @c: character being sought
+ * Return: a pointer to c if found, NULL if unfound
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	
+	return NULL;
+}
