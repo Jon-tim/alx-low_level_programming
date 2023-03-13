ebe1d946171c2ec8fcc272d6b37d3e24e2307503
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-13 14:31:44 -0700

    create an array of character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,12 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	char *array = malloc(size * sizeof(c));
+	char *arr = malloc(size * sizeof(c));
 
-	if (array == NULL)
+	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		array[i] = c;
+		arr[i] = c;
 
-	return (array);
+	return (arr);
 }
