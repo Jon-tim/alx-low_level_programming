f697614d6833eef68eb1ae7e143858e3d32ccef3
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-14 21:27:19 -0700

    concatenate program argument

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow - splits a string into words.
+ * @str: string to be splited
+ * Return: pointer of an array of integers
+ */
+char **strtow(char *str)
+{
+
+}
