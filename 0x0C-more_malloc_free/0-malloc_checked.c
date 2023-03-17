a534cd3f1c2b0703105af46ab58b97969a53847a
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-16 18:02:54 -0700

    Allocate memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 /**
  * malloc_checked - function that allocates memory using malloc
- * @b: parameter to be evaluated
+ * @b: amount of byte to be evaluated
  * Return: 98 if malloc fails or pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	char *ptr;
 
-	ptr  malloc(b);
+	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
 
