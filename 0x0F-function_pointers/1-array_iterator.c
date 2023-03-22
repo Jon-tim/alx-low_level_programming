1a2b439b8d46c4bb76ca54b60720e0c692f05e26
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-22 16:10:57 -0700

    execute a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers"
+/**
+ * array_iterator - function that executes a function given as
+ * a parameter on each element of an array
+ * @array: array which element is to be printed
+ * @size: size of the array
+ * @action: a pointer to the function to use
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
