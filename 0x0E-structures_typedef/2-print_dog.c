e59006ab2181ff43c2014cbf3f24019d8bbea296
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-03-21 18:42:24 -0700

    fix error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,8 +9,14 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		d->name == NULL ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
-		d->age == '\0' ? printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age == '\0')
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
