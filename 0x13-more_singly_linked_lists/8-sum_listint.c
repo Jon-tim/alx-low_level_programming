316e006f26b2a45e624c4dc2274e32104524710a
Jon-tim <awoniyitimi@outlook.com>
2023-04-04 16:59:29 -0700

    sum of values

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * sum_listint - sum all all the data (n) of a linked list
+ * @head: node
+ * Return: summation of all node values
+ */
+int sum_listint(listint_t *head)
+{
+	size_t sum = 0;
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
