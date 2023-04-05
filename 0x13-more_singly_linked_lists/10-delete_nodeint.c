b6780fcb01c5fff75bf5267b593fcd80aed76b86
Jon-tim <awoniyitimi@outlook.com>
2023-04-04 17:47:43 -0700

    delete given node index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	prevNode = *head;
 	if (index != 0)
 	{
-		for (i = 0; i < index - 1; i++)
+		for (i = 0; i < index - 1 && prevNode != NULL; i++)
 		{
 			prevNode = prevNode->next;
 		}
