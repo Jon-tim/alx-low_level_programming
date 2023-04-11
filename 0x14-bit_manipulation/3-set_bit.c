68d55d2495903a3d14d19eef8ab277c94955257f
Jon-tim <awoniyitimi@outlook.com>
2023-04-10 19:03:11 -0700

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * set_bit - set bit in a given index
+ * @n: number to convert to binary to set it's binary
+ * @index: given index
+ * Return: 1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n |= 1 << index;
+	return (1);
+}
