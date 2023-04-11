c49048ae345e044a866e9c1f8f4c5365d6205df3
Jon-tim <awoniyitimi@outlook.com>
2023-04-10 17:44:22 -0700

    update file to 3 functions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,8 +47,8 @@ void error_checks(int file_to, int file_from, char *argv[])
 int main(int argc, char *argv[])
 {
 	int err_from, err_to, file_from, file_to;
-	char *buffer[1024];
-	ssize_t read_from, write_to;
+	char buf[1024];
+	ssize_t read_f, write_to;
 
 	if (argc != 3)
 	{
@@ -58,22 +58,26 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_checks(file_to, file_from, argv);
-	read_from = 1024;
-	while (read_from == 1024)
+	read_f = 1024;
+	while (read_f == 1024)
 	{
-		read_from = read(file_from, buffer, 1024);
-		if (read_from == -1)
+		read_f = read(file_from, buf, 1024);
+		if (read_f == -1)
 		{
 			error_checks(file_to, file_from, argv);
 		}
-		write_to = write(file_to, buffer, read_from);
+		write_to = write(file_to, buf, read_f);
 		if (write_to == -1)
 		{
 			error_checks(file_to, file_from, argv);
 		}
 	}
 	if (file_to == -1)
+	{
 		error_checks(file_to, file_from, argv);
-	error_close(file_to, file_from);
+	}
+	err_from = close(file_from);
+	err_to = close(file_to);
+	error_close(err_to, err_from);
 	return (0);
 }
