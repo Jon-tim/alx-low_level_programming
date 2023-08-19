9cb8037a8b530eac28fd434010ed70c93f4ea9c7
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-18 19:01:43 -0700

    handle empty Head

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,6 +18,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = NULL;
 	dlistint_t *cur = NULL;
 
+	if (*h == NULL)
+		return (NULL);
 	if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
