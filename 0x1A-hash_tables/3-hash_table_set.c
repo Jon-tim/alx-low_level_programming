467e8f40ecb377149010f5c7892672513b7d4db0
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-09-08 16:54:06 -0700

    function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - a function that adds an element to the hash table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ * In case of collision, add the new node at the beginning of the list
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
