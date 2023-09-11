4b79585337049a219b147b343a0e00f89e79dcf9
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-09-10 18:39:40 -0700

    a function that prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - a function that prints a hash table
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *node;
+	int first_pair = 1;
+
+	if (ht == NULL)
+	{
+		printf("{}");
+		return;
+	}
+	printf("{");
+	while (index < ht->size)
+	{
+		node = ht->array[index];
+
+		while (node)
+		{
+			if (!first_pair)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first_pair = 0;
+			node = node->next;
+		}
+		index++;
+	}
+	printf("}\n");
+}
